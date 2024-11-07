b20788faaa2599a99d144bb969797dc1c7724687
ScenePlay <sceneplay2024@gmail.com>
2024-11-06 19:34:22 -0500

    Send in the files

--- supportFiles/setupAutoStart.sh
+++ supportFiles/setupAutoStart.sh
@@ -7,9 +7,12 @@ _exec="Exec=${_dir}"
 cp sceneplay.desktop.txt ~/.config/autostart/sceneplay.desktop
 cp ScenePlay.sh ~/Desktop
 sed -i "s|Exec=/something.sh|$_exec|g" ~/.config/autostart/sceneplay.desktop
+
+
 ##watchdog service
 mkdir -p ~/.config/systemd/user
 cp ~/ScenePlay/supportFiles/sceneplay_watchdog.service ~/.config/systemd/user/sceneplay_watchdog.service
+sed -i "s|userName|$USER|g" ~/.config/systemd/user/sceneplay_watchdog.service
 sudo chown $USER:$USER ~/ScenePlay/supportFiles/sceneplay_watchdog_service.py
 sudo chmod u+x ~/ScenePlay/supportFiles/sceneplay_watchdog_service.py
 loginctl enable-linger $USER
