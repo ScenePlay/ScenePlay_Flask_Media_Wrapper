52a06bca9d7c91d8867d7c8204e85977d4c2a61f
ScenePlay <sceneplay2024@gmail.com>
2024-12-16 10:36:09 -0500

    Update requirements.sh

--- requirements.sh
+++ requirements.sh
@@ -9,22 +9,27 @@ sudo apt-get -y install mpv
 sudo apt-get -y install sqlite3 
 sudo apt-get -y install libasound2-dev
 
-is_raspberry_pizero() {
+is_raspberry_pi() {
     local CPUINFO_PATH="/proc/cpuinfo"
     if [[ ! -e "$CPUINFO_PATH" ]]; then
         return 1
     fi
+    # The Raspberry Pi's architecture is armv6l or aarch64 (for newer models).
+    # The uname -m command returns the architecture of the system.
+    # The case statement checks if the result is either armv6l or aarch64.
+    # If it is, the function returns 0 (true), meaning this is a Raspberry Pi.
+    # If it is not, the function returns 1 (false), meaning this is not a Raspberry Pi.
     case "$(uname -m)" in
-        armv7l) return 0 ;;
+        armv6l|aarch64) return 0 ;;
         *) return 1 ;;
     esac
 }
 
 python3 -m venv ~/ScenePlay
 source ~/ScenePlay/bin/activate
-BREAK_SYSTEM_PACKAGES="--break-system-packages"
-if is_raspberry_pizero; then
-    BREAK_SYSTEM_PACKAGES=""
+BREAK_SYSTEM_PACKAGES=""
+if is_raspberry_pi; then
+    BREAK_SYSTEM_PACKAGES="--break-system-packages"
 fi
 pip3 install waitress $BREAK_SYSTEM_PACKAGES
 pip3 install pathlib $BREAK_SYSTEM_PACKAGES
